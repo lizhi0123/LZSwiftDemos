a5ef13b3f6d8b60636d4d6bdb623b0f47b4bba1c
LiZhi <zhang_zhi_li@163.com>
2023-12-05 14:19:48 +0800

    自定义转场动画

---
+++ LZTransitionDemo/LZTransitionDemo/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  LZTransitionDemo
+//
+//  Created by LiZhi on 2023/12/5.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+    }
+
+
+    @IBAction func presentClick(_ sender: Any) {
+        
+    }
+}
+
