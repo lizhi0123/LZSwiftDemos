019d5eaaf20705906ac77d33b9ed454d6c529918
LiZhi <zhang_zhi_li@163.com>
2024-05-24 12:38:41 +0800

    swiftUI study

---
+++ LZSwiftUIStudy/LZSwiftUIStudy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LZSwiftUIStudy
+//
+//  Created by LiZhi on 2024/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LZSwiftUIStudy/LZSwiftUIStudy/LZSwiftUIStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LZSwiftUIStudyApp.swift
+//  LZSwiftUIStudy
+//
+//  Created by LiZhi on 2024/5/24.
+//
+
+import SwiftUI
+
+@main
+struct LZSwiftUIStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
