312a6ba79af1c8260f31b83a796ced205cde441d
LiZhi <zhang_zhi_li@163.com>
2024-05-24 17:16:05 +0800

    地图信息完全显示

--- LZSwiftUIStudy/LZSwiftUIStudy/ListLocation/Views/LandmarkDetail.swift
+++ LZSwiftUIStudy/LZSwiftUIStudy/ListLocation/Views/LandmarkDetail.swift
@@ -18,7 +18,29 @@ struct LandmarkDetail: View {
             CircleImage(image: landmark.image)
                 .offset(y: -130)
                 .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text(landmark.name)
+                    .font(.title)
+
+                HStack {
+                    Text(landmark.park)
+                    Spacer()
+                    Text(landmark.state)
+                }
+                .font(.subheadline)
+                .foregroundStyle(.secondary)
+
+                Divider()
+
+                Text("About \(landmark.name)")
+                    .font(.title2)
+                Text(landmark.description)
+            }
+            .padding()
         }
+        .navigationTitle(landmark.name)
+        .navigationBarTitleDisplayMode(.inline)
         
     }
 }
--- LZSwiftUIStudy/LZSwiftUIStudy/ListLocation/Views/LandmarkList.swift
+++ LZSwiftUIStudy/LZSwiftUIStudy/ListLocation/Views/LandmarkList.swift
@@ -10,21 +10,16 @@ import SwiftUI
 struct LandmarkList: View {
     var body: some View {
         
-//        NavigationSplitView {
             List(landmarks) {landmark in
                 NavigationLink {
                     LandmarkDetail(landmark: landmark)
                 } label: {
                     LandmarkRow(landmark: landmark)
                 }
-
-
-            }.navigationTitle("Landmarks")
+            }
+            .navigationTitle("Landmarks")
+            .navigationBarTitleDisplayMode(.inline)
             
-//        } detail: {
-//            Text("Select a Landmark")
-//        }
-
     }
 }
 
