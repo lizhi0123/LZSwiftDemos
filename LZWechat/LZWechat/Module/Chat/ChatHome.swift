dfb5d7a7b73cceba7cf9789a324a0ccd91ca9191
LiZhi <zhang_zhi_li@163.com>
2024-05-30 18:31:30 +0800

    TAb value

---
+++ LZWechat/LZWechat/Module/Chat/ChatHome.swift
@@ -0,0 +1,18 @@
+//
+//  ChatHome.swift
+//  LZWechat
+//
+//  Created by LiZhi on 2024/5/30.
+//
+
+import SwiftUI
+
+struct ChatHome:View {
+    var body: some View{
+        Text("ChatHome")
+    }
+}
+
+#Preview {
+    ChatHome()
+}
--- LZWechat/LZWechat/Module/Root/RootView.swift
+++ LZWechat/LZWechat/Module/Root/RootView.swift
@@ -8,21 +8,31 @@
 import SwiftUI
 
 struct RootView: View {
+    
+    @State private var tabSection: Int = 0
     let datalist = 0..<5
     var body: some View {
+        TabView(selection: $tabSection,
+                content:  {
+            Text("Tab Content 1").tabItem { /*@START_MENU_TOKEN@*/Text("Tab Label 1")/*@END_MENU_TOKEN@*/ }.tag(1)
+            Text("Tab Content 2").tabItem { /*@START_MENU_TOKEN@*/Text("Tab Label 2")/*@END_MENU_TOKEN@*/ }.tag(2)
+            Text("Tab Content 3").tabItem { Text("Tab Label 3") }.tag(3)
+        })
+        /*
         NavigationView {
             List(0..<10) { item in
                 //Section(header: Text("特殊视图")) {
-//                    NavigationLink(
-//                        destination: WebViewPage(),
-//                        label: {
-//                            PageRow(title: "webView", subTitle: "用户展示一个打开的网页")
-//                        })
+                    NavigationLink(
+                        destination: Text("DEtial"),
+                        label: {
+                            Text("Label")
+                        })
                // }
-                Text("hello")
 
             }
-            .navigationTitle(Text("示例"))
+            .navigationTitle("微信")
+            .navigationBarTitleDisplayMode(.inline)
+            */
 //            .listStyle(GroupedListStyle())
 //            .navigationBarTitle(Text("Example"), displayMode: .inline)
 //            .navigationBarItems(trailing: Button(action: {
@@ -30,7 +40,7 @@ struct RootView: View {
 //            }, label: {
 //                Text("Right").foregroundColor(.orange)
 //            }))
-        }
+//        }
     }
 }
 
