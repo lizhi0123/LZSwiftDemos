018480b68dd2b5661316049a3a4502442a935bbc
LiZhi <zhang_zhi_li@163.com>
2024-08-06 19:23:17 +0800

    测试

--- DateFormatTest/DateFormatTest/DateFormateMoreController.swift
+++ DateFormatTest/DateFormatTest/DateFormateMoreController.swift
@@ -15,30 +15,30 @@ class DateFormateMoreController: UIViewController {
         self.title = "多个"
 
         
-        let button = UIButton(frame: CGRect(x: 20, y: 100, width: 140, height: 60))
+        let button = UIButton(frame: CGRect(x: 20, y: 100, width: 280, height: 60))
         button.setTitle("请点击测试DateFormate", for: .normal)
         button.backgroundColor = UIColor.red
-        button.addTarget(self, action: #selector(buttonClick), for: .touchUpInside)
+        button.addTarget(self, action: #selector(buttonMore1Click), for: .touchUpInside)
         self.view.addSubview(button)
         
-        let button2 = UIButton(frame: CGRect(x: 20, y: 200, width: 140, height: 60))
+        let button2 = UIButton(frame: CGRect(x: 20, y: 200, width: 180, height: 60))
         button2.setTitle("String 转 int", for: .normal)
         button2.backgroundColor = UIColor.red
-        button2.addTarget(self, action: #selector(button2Click), for: .touchUpInside)
+        button2.addTarget(self, action: #selector(buttonMore2Click), for: .touchUpInside)
         self.view.addSubview(button2)
         
     }
     
-    @objc func buttonClick(){
+    @objc func buttonMore1Click(){
         
-        for i in 0...1000 {
+        for i in 0...10000 {
             changeDate()
         }
     }
     
-    @objc func button2Click(){
+    @objc func buttonMore2Click(){
         
-        for i in 0...1000 {
+        for i in 0...10000 {
             stringToInt()
         }
     }
@@ -47,13 +47,11 @@ class DateFormateMoreController: UIViewController {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
         let date:Date? = dateFormatter.date(from: "2024-08-06")
-        print("-- date = ", date)
     }
     
     func stringToInt(){
         let str: String = "123"
         let it: Int? = Int(str)
-        print("--- it = ",it)
     }
     
 
--- DateFormatTest/DateFormatTest/DateFormateOnceController.swift
+++ DateFormatTest/DateFormatTest/DateFormateOnceController.swift
@@ -9,28 +9,74 @@ import UIKit
 
 class DateFormateOnceController: UIViewController {
 
+    let dateFormatter = DateFormatter()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("--- dateFormatter = ",dateFormatter)
 
         self.view.backgroundColor = UIColor.yellow
-        self.title = "单个"
+        self.title = "单个DataFormat"
         
-        let button = UIButton(frame: CGRect(x: 20, y: 100, width: 140, height: 60))
-        button.setTitle("请点击测试", for: .normal)
+        let button = UIButton(frame: CGRect(x: 20, y: 100, width: 280, height: 60))
+        button.setTitle("请点击测试DateFormate", for: .normal)
         button.backgroundColor = UIColor.red
-        button.addTarget(self, action: #selector(buttonClick), for: .touchUpInside)
+        button.addTarget(self, action: #selector(buttonOne1Click), for: .touchUpInside)
         self.view.addSubview(button)
+        
+        let button2 = UIButton(frame: CGRect(x: 20, y: 200, width: 280, height: 60))
+        button2.setTitle("仅 DateFormate初始化", for: .normal)
+        button2.backgroundColor = UIColor.red
+        button2.addTarget(self, action: #selector(buttonOne2Click), for: .touchUpInside)
+        self.view.addSubview(button2)
+
+        
+        let button3 = UIButton(frame: CGRect(x: 20, y: 300, width: 280, height: 60))
+        button3.setTitle("仅 DateFormate设置格式", for: .normal)
+        button3.backgroundColor = UIColor.red
+        button3.addTarget(self, action: #selector(buttonOne3Click), for: .touchUpInside)
+        self.view.addSubview(button3)
+        
+        let button4 = UIButton(frame: CGRect(x: 20, y: 400, width: 380, height: 60))
+        button4.setTitle("仅 DateFormate 转化", for: .normal)
+        button4.backgroundColor = UIColor.red
+        button4.addTarget(self, action: #selector(buttonOne4Click), for: .touchUpInside)
+        self.view.addSubview(button4)
+        
     }
     
-    @objc func buttonClick(){
-        self.changeDate()
+    @objc func buttonOne1Click(){
+        
+        for i in 0...10000 {
+            changeDate()
+        }
+    }
+    
+    ///仅初始化
+    @objc func buttonOne2Click(){
+        
+        for i in 0...10000 {
+            let dateFormatter2 = DateFormatter()
+        }
+    }
+    
+    @objc func buttonOne3Click(){
+        
+        for i in 0...10000 {
+            self.dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        }
+    }
+    
+    @objc func buttonOne4Click(){
+        
+        for i in 0...10000 {
+            let date:Date? = self.dateFormatter.date(from: "2024-08-06")
+        }
     }
     
     func changeDate(){
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
-        let date:Date? = dateFormatter.date(from: "2024-08-06")
-        print("-- date = ", date)
+        self.dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        let date:Date? = self.dateFormatter.date(from: "2024-08-06")
     }
 
 }
